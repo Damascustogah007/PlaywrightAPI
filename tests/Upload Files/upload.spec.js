83bc94256b43d453cbd5f669feb6cfc3c794c6a3
Damascsus Togah <Damascuscharles638@gmail.com>
2022-08-22 10:39:13 +0000

    Uploading files

---
+++ tests/Upload Files/upload.spec.js
@@ -0,0 +1,23 @@
+const { test } = require("@playwright/test");
+const baseURL = "https://www.sendgb.com/";
+
+test.describe("Uploading files", async () => {
+  const filePath0 = "file/raf2.txt";
+  const filePath1 = "file/raf2 - Copy.txt";
+  //Uploading files using  Input
+  test("Upload files using Input", async ({ page }) => {
+    await page.goto(baseURL);
+    await page.setInputFiles("input[name='qqfile']", [filePath0, filePath1]);
+    // await page.pause();
+  });
+
+  //Uploading files using
+  test.only("uploading files using chooser listerner", async ({ page }) => {
+    await page.goto("https://the-internet.herokuapp.com/upload");
+    await page.on("filechooser", async (filechooser) => {
+      await filechooser.setFiles([filePath0, filePath1]);
+    });
+    await page.click(".example + div#drag-drop-upload", { force: true });
+    await page.pause();
+  });
+});
